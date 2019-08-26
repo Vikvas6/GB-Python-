33a80c5997bcc7c69185d7e41881f13f85fe5054
vikvas <vikvas6@ya.ru>
2019-08-26 18:09:27 +0300

    Lesson 6 HW Decorating

--- server/echo/controllers.py
+++ server/echo/controllers.py
@@ -3,7 +3,7 @@ from protocol import make_response
 from decorators import logged
 
 
-@logged('%(name)s - %(response)s')
+@logged('%(name)s - %(request)s - %(response)s')
 def echo_controller(request):
     data = request.get('data')
     return make_response(200, request, data)
